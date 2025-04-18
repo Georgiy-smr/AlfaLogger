36a73402cc0c3cead24ea8120fe0d56baa3b041c
Gosha <mr.smirnovgd@yandex.ru>
2025-04-18 14:28:08 +0500

    Build

--- Solution/AlfaLoggerLib/Extension/AlfaLoggerRegistration.cs
+++ Solution/AlfaLoggerLib/Extension/AlfaLoggerRegistration.cs
@@ -1,21 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Reflection;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Reflection;
 using AlfaLoggerLib.Logging;
 using ContextEf;
-using Data.Entities;
-using MediatR;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
-using Repository;
-using Repository.Commands;
-using Repository.DtoObjects;
-using Serilog;
-using StatusGeneric;
-using Log = Serilog.Log;
 
 namespace AlfaLoggerLib.Extension
 {
